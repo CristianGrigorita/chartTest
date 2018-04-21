0f0257a2d8fdc433fb97a13a460860f9f8355e92
Cristian Grigorita <grigoria.c@gmail.com>
2018-04-21 04:05:49 +0300

    added AmCharts, work in progress

--- linegraph.js
+++ linegraph.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$.ajax({
-		url: "http://localhost/chart/data.php",
+		url: "http://localhost/chart/chartTest/data.php",
 		type: "GET",
 		success: function(data) {
 			console.log(data);
@@ -64,4 +64,151 @@ $(document).ready(function(){
 			console.log('error', data);
 		}
 	});
-});
+
+	var ctx = $('#huge-canvas');
+
+
+			for (var a=[],i=0;i<20;++i) a[i]=i;
+// http://stackoverflow.com/questions/962802#962890
+			function shuffle(array) {
+			  var tmp, current, top = array.length;
+			  if(top) while(--top) {
+			    current = Math.floor(Math.random() * (top + 1));
+			    tmp = array[current];
+			    array[current] = array[top];
+			    array[top] = tmp;
+			  }
+			  return array;
+			}
+
+			a = shuffle(a);
+
+	var myArr = Array.apply(null, Array(100)).map(function() { return Math.floor(Math.random() * 9 % 100); })
+	console.log(myArr)
+		var data = {
+			labels : myArr,
+			datasets: [
+			{
+				label: "TeamA score",
+				data: myArr,
+				backgroundColor: 'blue',
+				borderColor: 'lightblue',
+				fill: false,
+				lineTension: 0.2,
+				pointRadius: 5,
+			}]
+		}
+
+		var chart = new Chart(ctx, {
+			type: 'line',
+			data: data,
+			options: {}
+	});
+
+	//amCharts
+	var chartData = generateChartData();
+
+	var chart = AmCharts.makeChart("chartdiv",{
+		"type": "serial",
+  		"theme" : "light",
+  		"mouseWheelZoomEnabled":true,
+		"legend": {
+  			"useGraphSettings": true
+  		},
+  		"dataProvider": chartData,
+  		"synchronizeGrid":true,
+  		"valueAxes": [{
+    		'id':'v1',
+    		'axisColor':'#FF6600',
+    		'axisThickness': 2,
+    		'axisAlpha': 1,
+    		'position': 'right'
+    	}, {
+ 			"id":"v2",
+        	"axisColor": "#FCD202",
+        	"axisThickness": 2,
+        	"axisAlpha": 1,
+        	"position": "left"
+    	}],
+    	"graphs": [{
+	    	"valueAxis": "v1",
+	        "lineColor": "#FF6600",
+	        "bullet": "round",
+	        "bulletBorderThickness": 1,
+	        "hideBulletsCount": 30,
+	        "title": "benchmark",
+	        "valueField": "benchAmptitude",
+			"fillAlphas": 0
+    	},
+    	{
+	    	"valueAxis": "v2",
+	        "lineColor": "#FCD202",
+	        "bullet": "square",
+	        "bulletBorderThickness": 1,
+	        "hideBulletsCount": 30,
+	        "title": "patient",
+	        "valueField": "amptitude",
+			"fillAlphas": 0	
+    	}],
+    	"chartScrollbar": {},
+		"chartCursor": {
+		   	"pan": true,
+		    "valueLineEnabled": true,
+		    "valueLineBalloonEnabled": true,
+		    "cursorAlpha":1,
+		    "cursorColor":"#258cbb",
+		    "valueLineAlpha":0.2,
+		   	"valueZoomable":true
+		},
+		"categoryField": "time",
+		"categoryAxis": {
+   			"gridPosition": "start"
+  		},
+  		"export": {
+    		"enabled" : true,
+    		"postion" : "bottom-right"
+		},
+		"marginRight": 40,
+		"marginLeft": 40,
+		"autoMarginOffset": 20,
+	   	"balloon": {
+	  	     "borderThickness": 1,
+	   	     "shadowAlpha": 0
+	   	 },
+	    "titles": [{
+     	 		"size": 15,
+     	 		"text": "Sacade"
+     	 	}],
+	    "valueScrollbar":{
+	      "oppositeAxis":false,
+	      "offset":50,
+	      "scrollbarHeight":10
+	    }
+	});
+
+	chart.addListener("dataUpdated", zoomChart);
+	zoomChart();
+
+	function generateChartData(){
+		var time = "33:12";
+		var benchAmptitude = 2;
+		var amptitude = 12;
+
+		time += "53:13";
+		benchAmptitude += 4;
+		amptitude += 201;
+
+		var chartData = [];
+		chartData.push({
+			time: time,
+			benchAmptitude: benchAmptitude,
+			amptitude: amptitude
+		});
+
+		return chartData;
+	};
+	
+	function zoomChart(){
+    	chart.zoomToIndexes(chart.dataProvider.length - 20, chart.dataProvider.length - 1);
+	};
+})
